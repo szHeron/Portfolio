bb4281c19b90000194bd2d7c444025484b1628e6
szHeron <heronbr7@gmail.com>
2021-09-13 20:14:31 -0300

    sendmail added

--- pages/feedback.js
+++ pages/feedback.js
@@ -127,7 +127,7 @@ export default function Feedback(){
 
     function handleSubmit(){
         if(validate()){
-            fetch('http://localhost:3000/api/sendMail',{
+            fetch('https://portfolio-szheron.vercel.app/api/sendMail',{
                 method:'POST',
                 headers:{
                     'Content-type': 'application/json'
