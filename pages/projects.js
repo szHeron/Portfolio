458bbf24249181c648d0d7b58bb65e497244679e
szHeron <heronbr7@gmail.com>
2021-10-26 17:45:24 -0300

    added new repos

--- pages/projects.js
+++ pages/projects.js
@@ -38,6 +38,8 @@ export default function Projects({repositories}){
             <Project title={repos.names[1]} desc={repos.descs[1]} link={repos.links[1]} topics={repos.topics[1]}/>
             <Project title={repos.names[2]} desc={repos.descs[2]} link={repos.links[2]} topics={repos.topics[2]}/>
             <Project title={repos.names[3]} desc={repos.descs[3]} link={repos.links[3]} topics={repos.topics[3]}/>
+            <Project title={repos.names[4]} desc={repos.descs[4]} link={repos.links[4]} topics={repos.topics[4]}/>
+            <Project title={repos.names[5]} desc={repos.descs[5]} link={repos.links[5]} topics={repos.topics[5]}/>
             <Copy style={{marginTop: '25px',position: 'relative'}}/>
         </Content>
     )
