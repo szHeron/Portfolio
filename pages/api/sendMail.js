b1c6e2a09623358a04c67a900c400afe2a6d48b3
szheron <heronbr7@gmail.com>
2022-09-06 13:27:23 -0300

    added json parse

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -2,30 +2,31 @@ const nodemailer = require('nodemailer');
 
 export default function sendMail(req,res){
     const transporter = nodemailer.createTransport({
-        host: "in-v3.mailjet.com",
+        host: 'in-v3.mailjet.com',
         port: 587,
-        secure : false,
         auth: {
             user: process.env.USERMAIL,
             pass: process.env.PASSMAIL
-        },
-        tls: {
-            rejectUnauthorized: false
         }
     });
+    const data = JSON.parse(req.body);
 
     try{
+        const email = data.email;
+        const message = data.message;
+
         transporter.sendMail({
             from: `<contatoheron.dev@outlook.com>`,
-            to: "contatoheron.dev@gmail.com, Mailer <contatoheron.dev@gmail.com>",
-            subject: `${req.body.email} mandou um feedback pelo site portfolio`,
-            text: `${req.body.message}`
-        })
-        res.json({sucess: true})
+            to: 'contatoheron.dev@gmail.com, Mailer <contatoheron.dev@gmail.com>',
+            subject: `${email} mandou um feedback pelo site portfolio`,
+            text: `${message}`
+        });
+
+        res.json({sucess: true});
         res.status(200).end();
     }catch(err){
         console.log(err);
-        res.json({sucess: false, err: err})
+        res.json({sucess: false, err: err});
         res.status(500).end();
     }
 }
